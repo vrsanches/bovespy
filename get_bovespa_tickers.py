e9b9070aea845ed08a40c4ffba634912d467906f
Vinicius Sanches <vr.sanches@gmail.com>
2017-05-07 17:51:39 -0300

    First Function to get Bovespa tickers
    Get BOVESPA tickers and leaves them ready to be used with yahoo api, appending ticker.SA (PETR4.SA) at the end, so it can be properly read.

---
+++ get_bovespa_tickers.py
@@ -0,0 +1,30 @@
+import string
+import bs4 as bs
+import requests
+import pickle
+
+def get_bovespa_tickers():
+	print ("Saving All BOVESPA tickers...")
+
+	for letter in range(24):
+		#	Goes from 0 to 24 because there are no brazilian stocks beginning
+		#	with Y or Z (weird, right? Took me 10 minutes to find out what was
+		#	wrong)
+		resp = requests.get('https://br.advfn.com/bolsa-de-valores/bovespa/'+string.ascii_uppercase[letter])
+		soup = bs.BeautifulSoup(resp.text, "lxml")
+		table = soup.find('table', {'class':'atoz-link-bov'})
+		tickers = []
+		for row in table.findAll('tr')[1:]:
+			ticker = row.findAll('td')[1].text
+			tickers.append(ticker+'.SA')
+
+	with open("IBOVESPAtickers.pickle", "wb") as f:
+		pickle.dump(tickers,f)
+		print("File IBOVESPAtickers.pickle created")	# Just for Debug
+	return tickers
+
+
+
+bovespa_tickers = get_bovespa_tickers()
+
+	
